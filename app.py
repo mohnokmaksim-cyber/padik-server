d24fe32684a2e6a407b40e677ef298f16d7b5ca9
mohnokmaksim-cyber <mohnokmaksim@gmail.com>
2026-04-24 03:06:16 +0300

    Fix formatting issue in app.py

--- app.py
+++ app.py
@@ -573,4 +573,4 @@ def search_users():
         return response, 500
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000) 
